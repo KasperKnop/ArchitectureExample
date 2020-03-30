e7adf03063c123fd9bd4f7f2f9bb03d5d331586c
KasperKnop <kasperknop@gmail.com>
2020-03-30 10:46:33 +0200

    Updated dependencies

--- app/src/main/java/com/example/kaspe/architectureexample/MainActivity.java
+++ app/src/main/java/com/example/kaspe/architectureexample/MainActivity.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import java.util.List;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private NoteViewModel noteViewModel;
@@ -21,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         editText = findViewById(R.id.editText);
         textView = findViewById(R.id.textView);
 
-        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
@@ -38,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void saveNote(View v) {
-        noteViewModel.insert(new Note(editText.getText().toString(),"description",1));
+        noteViewModel.insert(new Note(editText.getText().toString(), "description", 1));
     }
 
     public void deleteAllNotes(View v) {
