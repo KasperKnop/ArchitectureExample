82c53c27f99698d730d6a30b0376e8acd7f4200e
Kasper Knop Rasmussen <kasperknop@gmail.com>
2021-04-09 12:26:20 +0200

    Cleaned code a bit

--- app/src/main/java/com/example/kaspe/architectureexample/MainActivity.java
+++ app/src/main/java/com/example/kaspe/architectureexample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.kaspe.architectureexample;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
